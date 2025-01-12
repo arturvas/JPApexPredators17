4bbdd2a0d461b3015bdf6233162b1052f78eae42
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2025-01-12 15:59:23 -0400

    feat: add a new var to be the selected Apex Type when filter

--- JPApexPredators17/Predators.swift
+++ JPApexPredators17/Predators.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 class Predators {
+    var allApexPredators: [ApexPredator] = []
+    
 //    the type will be a collection of apex predators
     var apexPredators: [ApexPredator] = []
     
@@ -23,9 +25,10 @@ class Predators {
                 print("Loaded data with success: \(data.count) bytes")
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
-                apexPredators = try decoder.decode([ApexPredator].self, from: data)
-                print("Successfull decode: \(apexPredators.count) predators loaded.")
-                
+                allApexPredators = try decoder.decode([ApexPredator].self, from: data)
+                print("Successfull decode: \(allApexPredators.count) predators loaded.")
+                apexPredators = allApexPredators
+                print("Apex Predators populated \(apexPredators.count) predators")
             } catch {
                 print("Error decoding JSON data: \(error)")
             }
@@ -47,6 +50,16 @@ class Predators {
     func sort(by alphabetical: Bool){
         apexPredators.sort { predator1, predator2 in
             alphabetical ? predator1.name < predator2.name : predator2.id < predator1.id
+        }
+    }
+    
+    func filter(by type: APType) {
+        if type == .all {
+            apexPredators = allApexPredators
+        } else {
+            apexPredators = allApexPredators.filter { predator in
+                predator.type == type
+            }
         }
     }
 }
