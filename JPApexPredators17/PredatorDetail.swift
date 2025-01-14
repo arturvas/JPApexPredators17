7e4bc4abc97c69784e481d8addb55a45c6cac7cd
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2025-01-13 23:29:24 -0400

    feat: add component for Predator Details

--- JPApexPredators17/PredatorDetail.swift
+++ JPApexPredators17/PredatorDetail.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct PredatorDetail: View {
+    let predator: ApexPredator
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            ScrollView{
+                ZStack(alignment: .bottomTrailing){
+                    
+//                    BackGround image
+                    Image(predator.type.rawValue)
+                        .resizable()
+                        .scaledToFit()
+                    
+//                    Dino Image
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width/1.5, height: geo.size.height/3.7)
+                        .scaleEffect(x: -1)
+                        .shadow(color: .black, radius: 7)
+                        .offset(y: 20)
+                    
+                    
+                }
+                
+//                    Dino Image
+//
+//                    Current location
+//
+//                    Appears in
+//
+//                    Movie moments
+//
+//                    Link to webpage
+                
+            }
+        }
+        .ignoresSafeArea(.all)
     }
 }
 
 #Preview {
-    PredatorDetail()
+    PredatorDetail(predator: Predators().apexPredators[2])
+//        .preferredColorScheme(.dark)
 }
