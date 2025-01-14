a823bba10177a11e696119cb580206f1738cadb1
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2025-01-13 23:28:51 -0400

    refactor: extract component of PredatorDetail

--- JPApexPredators17/ContentView.swift
+++ JPApexPredators17/ContentView.swift
@@ -27,9 +27,7 @@ struct ContentView: View {
         NavigationStack{
             List(filteredDinos) { predator in
                 NavigationLink {
-                    Image(predator.image)
-                        .resizable()
-                        .scaledToFit()
+                    PredatorDetail(predator: predator)
                 } label: {
                     HStack{
                         
